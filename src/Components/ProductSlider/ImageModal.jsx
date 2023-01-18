8f1c83a9f8d46f084f15c5824b70f79c643662b8
aliibnemasud <mdmasudrony@gmail.com>
2023-01-19 02:04:46 +0600

    modal are working

---
+++ src/Components/ProductSlider/ImageModal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ImageModal = ({changeState,openModal, img}) => {
+    let active = openModal;
+
+  return (
+    <div className={`my-modal ${active}`}>
+      <span onClick={changeState} class="close-btn">
+        &times;
+      </span>
+      <img width="20%" src={img} className="myModal-content" />
+      <div id="caption"></div>
+    </div>
+  );
+};
+
+export default ImageModal;
--- src/Components/ProductSlider/NewProductSlider.jsx
+++ src/Components/ProductSlider/NewProductSlider.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import React, { Component } from "react";
 import { FaAlgolia, FaArrowCircleLeft, FaArrowCircleRight, FaEye } from "react-icons/fa";
 import Slider from "react-slick";
+import ImageModal from "./ImageModal";
+// import '../../Pages/Products/ProductCard.css';
 
 /* function SampleNextArrow(props) {
   const { className, style, onClick } = props;
@@ -23,19 +25,23 @@ class NewProductSlider extends Component {
     const products = data?.data;
   }; */
 
-  state = { products: [], openModal: "" };
+  state = { products: [], openModal: "", pdImg: '' };
 
   componentDidMount() {
     axios.get("/data/products.json").then((res) => this.setState({ products: res.data }));
   }
 
-  handleOpenModal = () => {
+  /* handleOpenModal = () => {
     if (this.state.openModal === "d-block") {
       this.setState({ openModal: "" });
     } else {
       this.setState({ openModal: "d-block" });
     }
-  };
+  }; */
+
+  changeState = (props) => {
+    this.setState({ openModal: props })
+  }
 
   render() {
     var settings = {
@@ -85,7 +91,15 @@ class NewProductSlider extends Component {
               <div className="product-card col-lg-3 my-3">
                 <div className="card-content rounded">
                   <div className="card-image">
-                    <img onClick={this.handleOpenModal} src={product.img[0]} className="img-fluid text-center" alt="" />
+                    <img onClick={() => {
+                      if (this.state.openModal === "d-block") {
+                        this.setState({ openModal: "" });
+                      } else {
+                        this.setState({ 
+                          openModal: "d-block",
+                          pdImg: product.img[0] });
+                      }
+                    }} src={product.img[0]} className="img-fluid text-center" alt="" />
                     <div className="card-bottom py-2 w-100 d-flex justify-content-around">
                       <button className="btn btn-lite text-danger">
                         <FaEye />
@@ -103,18 +117,16 @@ class NewProductSlider extends Component {
                     <h5>Kod: OW1818</h5>
                   </div>
                 </div>
-
-                <div className={`my-modal ${this.state.openModal}`}>
-                  <span onClick={this.handleOpenModal} class="close-btn">
-                    &times;
-                  </span>
-                  <img width="20%" src={product.img[0]} className="myModal-content" />
-                  <div id="caption"></div>
-                </div>
               </div>
             );
           })}
         </Slider>
+
+
+        {
+          this.state.openModal === 'd-block' && <ImageModal changeState={this.changeState} openModal={this.state.openModal} img={this.state.pdImg} />
+        }
+
       </div>
     );
   }
